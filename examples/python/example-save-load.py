aaa468ce4d4fd5a38172f7ee60bd2c56bcf42ef1
PotatoSpudowski <bahushruth.bahushruth@gmail.com>
2023-04-17 15:39:42 +0530

    added saving and loading examples + refactoring

---
+++ examples/python/example-save-load.py
@@ -0,0 +1,54 @@
+from build.fastllama import Model, ModelKind
+
+MODEL_PATH = "./models/ALPACA-LORA-7B/alpaca-lora-q4_0.bin"
+
+def stream_token(x: str) -> None:
+    """
+    This function is called by the llama library to stream tokens
+    """
+    print(x, end='', flush=True)
+
+model = Model(
+        id=ModelKind.ALPACA_LORA_7B,
+        path=MODEL_PATH, #path to model
+        num_threads=16, #number of threads to use
+        n_ctx=512, #context size of model
+        last_n_size=16, #size of last n tokens (used for repetition penalty) (Optional)
+        n_batch=128,
+    )
+
+print("")
+print("Start of chat (type 'exit' to exit)")
+print("")
+
+while True:
+    user_input = input("User: ")
+
+    if user_input == "exit":
+        break
+
+    if user_input == "save":
+        model.save_state("models/session1.bin")
+        continue
+
+    if user_input == "load":
+        model.load_state("models/session1.bin")
+        continue
+
+    user_input = "\n\n### Instruction:\n\n" + user_input + "\n\n### Response:\n\n"
+
+    res = model.ingest(user_input)
+
+    if res != True:
+        break
+    
+    print("\n")
+
+    res = model.generate(
+        num_tokens=100, 
+        top_p=0.95, #top p sampling (Optional)
+        temp=0.8, #temperature (Optional)
+        repeat_penalty=1.0, #repetition penalty (Optional)
+        streaming_fn=stream_token, #streaming function
+        stop_words=["###"] #stop generation when this word is encountered (Optional)
+        )
--- interfaces/python/fastllama.py
+++ interfaces/python/fastllama.py
@@ -11,19 +11,46 @@ def get_library_path(*args) -> str:
     return os.path.join(*args, LIBRARY_NAME)
 
 class Logger:
+    """
+    Logger class for reporting messages.
+    """
     def log_info(self, func_name: str, message: str) -> None:
+        """
+        Logs informational messages.
+
+        :param func_name: Name of the function that generated the log message.
+        :param message: The log message.
+        """
         print(f"[Info]: Func('{func_name}') {message}", flush=True, end='')
     
     def log_err(self, func_name: str, message: str) -> None:
+        """
+        Logs error messages.
+
+        :param func_name: Name of the function that generated the log message.
+        :param message: The log message.
+        """
         print(f"[Error]: Func('{func_name}') {message}", flush=True, end='')
     
     def log_warn(self, func_name: str, message: str) -> None:
+        """
+        Logs warning messages.
+
+        :param func_name: Name of the function that generated the log message.
+        :param message: The log message.
+        """
         print(f"[Warn]: Func('{func_name}') {message}", flush=True, end='')
     
     def reset(self) -> None:
+        """
+        Resets the logger state.
+        """
         return None
 
 class ModelKind(Enum):
+    """
+    Enumeration of supported model kinds.
+    """
     LLAMA_7B = "LLAMA-7B"
     LLAMA_13B = "LLAMA-13B"
     LLAMA_30B = "LLAMA-30B"
@@ -37,6 +64,9 @@ C_LLAMA_LOGGER_FUNC = ctypes.CFUNCTYPE(None, ctypes.c_char_p, ctypes.c_int, ctyp
 C_LLAMA_LOGGER_RESET_FUNC = ctypes.CFUNCTYPE(None)
 
 class c_llama_logger(ctypes.Structure):
+    """
+    C-compatible logger structure.
+    """
     _fields_ = [
         ('log', C_LLAMA_LOGGER_FUNC),
         ('log_err', C_LLAMA_LOGGER_FUNC),
@@ -45,12 +75,18 @@ class c_llama_logger(ctypes.Structure):
     ]
 
 class llama_array_view_f(ctypes.Structure):
+    """
+    C-compatible array view structure for floats.
+    """
     _fields_ = [
         ('data', ctypes.POINTER(ctypes.c_float)),
         ('size', ctypes.c_size_t),
     ]
 
 class c_llama_model_context_args(ctypes.Structure):
+    """
+    C-compatible model context arguments structure.
+    """
     _fields_ = [
         ('embedding_eval_enabled', ctypes.c_bool),
         ('should_get_all_logits', ctypes.c_bool),
@@ -65,37 +101,72 @@ class c_llama_model_context_args(ctypes.Structure):
     ]
 
 class c_llama_model_context(ctypes.Structure):
+    """
+    C-compatible model context structure.
+    """
     pass
 
 c_llama_model_context_ptr = ctypes.POINTER(c_llama_model_context)
 
 def make_c_logger_func(func: Callable[[str, str], None]) -> Any:
+    """
+    Creates a C-compatible logger function from a Python callable.
+
+    :param func: Python callable to be converted to a C-compatible logger function.
+    :return: C-compatible logger function.
+    """
     def c_logger_func(func_name: ctypes.c_char_p, func_name_len: ctypes.c_int, message: ctypes.c_char_p, message_len: ctypes.c_int) -> None:
         func(ctypes.string_at(func_name, int(func_name_len)).decode('utf-8'), ctypes.string_at(message, int(message_len)).decode('utf-8'))
     return C_LLAMA_LOGGER_FUNC(c_logger_func)
 
 def make_c_logger_reset_func(func: Callable[[], None]) -> Any:
+    """
+    Creates a C-compatible logger reset function from a Python callable.
+
+    :param func: Python callable to be converted to a C-compatible logger reset function.
+    :return: C-compatible logger reset function.
+    """
     def c_logger_func() -> None:
         func()
     return C_LLAMA_LOGGER_RESET_FUNC(c_logger_func)
 
 class Model:
+    """
+    Model class for interacting with the language model.
+    """
     def __init__(
         self,
-        id: Union[str, ModelKind], # Model Id. See the readme for support models and get the id
-        path: str, # File path to the model
-        num_threads: int = multiprocessing.cpu_count(), # Number of threads to use during evaluation of model
-        n_ctx: int = 512, # Size of the memory context to use
-        last_n_size: int = 64, # Number of token that the model can remember
-        seed: int = 0, # Random number seed to be used in model
-        tokens_to_keep: int = 200, # Number of tokens to keep when tokens are removed from buffer to save memory
-        n_batch: int = 16, # Size of the token batch that will be processed at a given time
+        id: Union[str, ModelKind], 
+        path: str, 
+        num_threads: int = multiprocessing.cpu_count(), 
+        n_ctx: int = 512, 
+        last_n_size: int = 64, 
+        seed: int = 0, 
+        tokens_to_keep: int = 200,
+        n_batch: int = 16, 
         should_get_all_logits: bool = False,
         embedding_eval_enabled: bool = False,
         allocate_extra_mem: int = 0,
-        logger: Optional[Logger] = None, # Logger to be used for reporting messages
+        logger: Optional[Logger] = None, 
         library_path = get_library_path('build', 'interfaces','python')
         ):
+        """
+        Initializes a new model instance.
+
+        :param id: Model identifier. Can be a string or a ModelKind enum value.
+        :param path: Path to the model file.
+        :param num_threads: Number of threads to use during model evaluation. Default is the number of CPU cores.
+        :param n_ctx: Size of the memory context to use. Default is 512.
+        :param last_n_size: Number of tokens the model can remember. Default is 64.
+        :param seed: Random number seed to be used in the model. Default is 0.
+        :param tokens_to_keep: Number of tokens to keep when tokens are removed from the buffer to save memory. Default is 200.
+        :param n_batch: Size of the token batch that will be processed at a given time. Default is 16.
+        :param should_get_all_logits: Flag to indicate if all logit values should be returned. Default is False.
+        :param embedding_eval_enabled: Flag to enable embedding evaluation. Default is False.
+        :param allocate_extra_mem: Amount of extra memory to allocate. Default is 0.
+        :param logger: Logger instance to be used for reporting messages. Default is None.
+        :param library_path: Path to the library file. Default is the result of get_library_path('build', 'interfaces','python').
+        """
 
         self.lib = ctypes.cdll.LoadLibrary(library_path)
 
@@ -137,29 +208,59 @@ class Model:
             raise RuntimeError("Unable to load model")
 
     def __get_default_ctx_args__(self) -> c_llama_model_context_args:
+        """
+        Retrieves the default model context arguments.
+
+        :return: Default model context arguments.
+        """
         fn = self.lib.llama_create_default_context_args
         fn.restype = c_llama_model_context_args
         return fn()
     
     def __create_model_ctx__(self, ctx_args: c_llama_model_context_args):
+        """
+        Creates a new model context.
+
+        :param ctx_args: Model context arguments.
+        :return: Model context pointer.
+        """
         fn = self.lib.llama_create_context
         fn.restype = c_llama_model_context_ptr
         fn.argtypes = [c_llama_model_context_args]
         return fn(ctx_args)
     
     def save_state(self, filepath: str) -> bool:
+        """
+        Saves the current model state to a file.
+
+        :param filepath: Path to the file where the model state will be saved.
+        :return: True if successful, False otherwise.
+        """
         fn = self.lib.llama_save_state
         fn.argtypes = [c_llama_model_context_ptr, ctypes.c_char_p]
         fn.restype = ctypes.c_bool
         return bool(fn(self.ctx, bytes(filepath, 'utf-8')))
     
     def load_state(self, filepath: str) -> bool:
+        """
+        Loads a model state from a file.
+
+        :param filepath: Path to the file containing the model state to load.
+        :return: True if successful, False otherwise.
+        """
         fn = self.lib.llama_load_state
         fn.argtypes = [c_llama_model_context_ptr, ctypes.c_char_p]
         fn.restype = ctypes.c_bool
         return bool(fn(self.ctx, bytes(filepath, 'utf-8')))
 
     def ingest(self, prompt: str, is_system_prompt: bool = False) -> bool:
+         """
+        Ingests a prompt into the model.
+
+        :param prompt: The prompt to be ingested.
+        :param is_system_prompt: Flag to indicate if the prompt is a system prompt. Default is False.
+        :return: True if successful, False otherwise.
+        """
         if is_system_prompt:
             ingest_fn = self.lib.llama_ingest_system_prompt
         else:
@@ -172,13 +273,25 @@ class Model:
     def generate(
             self,
             streaming_fn=Callable[[str], None],
-            num_tokens: int = 100, # Max number of tokens that will be generated by the model
-            top_k: int = 40, # Controls the diversity by limiting the selection to the top k highest probability tokens
-            top_p: float = .95, # Filters out tokens based on cumulative probability, further refining diversity
-            temp: float = .8, # Adjusts the sampling temperature, influencing creativity and randomness
-            repeat_penalty: float = 1.0, # Penalizes repeated tokens to reduce redundancy in generated text
-            stop_words: List[str] = [], # Words that will stop the generate function when it encounters them inside the token buffer
+            num_tokens: int = 100, 
+            top_k: int = 40, 
+            top_p: float = .95, 
+            temp: float = .8, 
+            repeat_penalty: float = 1.0, 
+            stop_words: List[str] = [], 
         ) -> bool:
+        """
+        Generates text using the model.
+
+        :param streaming_fn: Function to be called with the generated text.
+        :param num_tokens: Maximum number of tokens to be generated by the model. Default is 100.
+        :param top_k: Controls the diversity by limiting the selection to the top k highest probability tokens. Default is 40.
+        :param top_p: Filters out tokens based on cumulative probability, further refining diversity. Default is 0.95.
+        :param temp: Adjusts the sampling temperature, influencing creativity and randomness. Default is 0.8.
+        :param repeat_penalty: Penalizes repeated tokens to reduce redundancy in generated text. Default is 1.0.
+        :param stop_words: List of words that will stop the generate function when encountered in the token buffer. Default is an empty list.
+        :return: True if successful, False otherwise.
+        """
         def callback_fn(token: ctypes.c_char_p, len: ctypes.c_int):
             arr = ctypes.string_at(token, int(len))
             streaming_fn(arr.decode('utf-8'))
@@ -212,6 +325,12 @@ class Model:
         ))
     
     def perplexity(self, prompt: str) -> Optional[float]:
+        """
+        Calculates the perplexity of a given prompt.
+
+        :param prompt: The prompt to calculate perplexity for.
+        :return: Perplexity value if successful, None otherwise.
+        """
         per_fun = self.lib.llama_perplexity
         per_fun.restype = ctypes.c_float
         per_fun.argtypes = [c_llama_model_context_ptr, ctypes.c_char_p]
@@ -221,6 +340,11 @@ class Model:
         return res
     
     def get_embeddings(self) -> List[float]:
+        """
+        Retrieves the embeddings of the model.
+
+        :return: List of embeddings.
+        """
         getter = self.lib.llama_get_embeddings
         getter.restype = llama_array_view_f
         getter.argtypes = [c_llama_model_context_ptr]
@@ -228,6 +352,11 @@ class Model:
         return res.data[:int(res.size)]
     
     def get_logits(self) -> List[float]:
+        """
+        Retrieves the logits of the model.
+
+        :return: List of logits.
+        """
         getter = self.lib.llama_get_logits
         getter.restype = llama_array_view_f
         getter.argtypes = [c_llama_model_context_ptr]
@@ -235,6 +364,9 @@ class Model:
         return res.data[:int(res.size)]
     
     def __del__(self):
+        """
+        Destructor for the Model class.
+        """
         signal.signal(signal.SIGINT, signal.SIG_DFL)
         signal.siginterrupt(signal.SIGINT, False)
         lib = self.lib
