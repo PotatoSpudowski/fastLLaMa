b53e7882d132fde601567962a80f59d9f3e6baab
Amit Singh <amitsingh19975@gmail.com>
2023-04-24 19:54:04 +0530

    set cuda flags

--- cmake/Cuda.cmake
+++ cmake/Cuda.cmake
@@ -11,7 +11,10 @@ function(set_cuda project_name)
             message(STATUS "cuBLAS found")
 
             enable_language(CUDA)
-            
+
+            set(_CMAKE_CUDA_WHOLE_FLAG "-c")
+            set(CMAKE_CUDA_COMPILE_OBJECT ${CMAKE_CUDA_COMPILE_OBJECT} -Xcompiler -fPIC)
+
             target_sources(${project_name} PRIVATE ${CMAKE_SOURCE_DIR}/lib/ggml-cuda.cu ${CMAKE_SOURCE_DIR}/include/ggml-cuda.h)
             target_link_libraries(${project_name} PRIVATE CUDA::cudart_static CUDA::cublas_static CUDA::cublasLt_static)
             target_compile_definitions(${project_name} PRIVATE "GGML_USE_CUBLAS")
