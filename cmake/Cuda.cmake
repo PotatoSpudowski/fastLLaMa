d08d6c548d70193f0fd570ea5dbfe69437c867c4
PotatoSpudowski <bahushruth.bahushruth@gmail.com>
2023-04-24 20:01:58 +0530

    Bug fix + Added cuda files

--- cmake/Cuda.cmake
+++ cmake/Cuda.cmake
@@ -1,3 +1,10 @@
+set(_CMAKE_CUDA_WHOLE_FLAG "-c")
+
+if(NOT CMAKE_CUDA_COMPILE_OBJECT)
+  set(CMAKE_CUDA_COMPILE_OBJECT
+    "<CMAKE_CUDA_COMPILER> ${_CMAKE_CUDA_EXTRA_FLAGS} <DEFINES> <INCLUDES> <FLAGS> ${_CMAKE_COMPILE_AS_CUDA_FLAG} <CUDA_COMPILE_MODE> <SOURCE> -o <OBJECT>")
+endif()
+
 option(LLAMA_CUBLAS "fastllama: use cuBLAS" ON)
 
 function(set_cuda project_name)
@@ -11,9 +18,6 @@ function(set_cuda project_name)
             message(STATUS "cuBLAS found")
 
             enable_language(CUDA)
-
-            set(_CMAKE_CUDA_WHOLE_FLAG "-c")
-            set(CMAKE_CUDA_COMPILE_OBJECT ${CMAKE_CUDA_COMPILE_OBJECT} -Xcompiler -fPIC)
 
             target_sources(${project_name} PRIVATE ${CMAKE_SOURCE_DIR}/lib/ggml-cuda.cu ${CMAKE_SOURCE_DIR}/include/ggml-cuda.h)
             target_link_libraries(${project_name} PRIVATE CUDA::cudart_static CUDA::cublas_static CUDA::cublasLt_static)
