02543915e16cd9cccca5c71eb19f3370f92129a0
Amit Singh <amitsingh19975@gmail.com>
2023-04-24 19:28:27 +0530

    update cuda cmake

--- cmake/Cuda.cmake
+++ cmake/Cuda.cmake
@@ -1,9 +1,20 @@
+option(LLAMA_CUBLAS "fastllama: use cuBLAS" ON)
+
 function(set_cuda project_name)
-    find_package(CUDAToolkit QUIET)
-    if(BLAS_FOUND)
-        target_link_libraries(${project_name} PRIVATE CUDA::cublas)
-        target_compile_definitions(${project_name} PRIVATE "GGML_USE_CUBLAS")
-    endif(BLAS_FOUND)
+
+    if(LLAMA_CUBLAS)
+        find_package(CUDAToolkit)
+        if (CUDAToolkit_FOUND)
+            message(STATUS "cuBLAS found")
+
+            enable_language(CUDA)
+
+            target_link_libraries(${project_name} PRIVATE ggml-cuda.cu ggml-cuda.h CUDA::cudart_static CUDA::cublas_static CUDA::cublasLt_static)
+            target_compile_definitions(${project_name} PRIVATE "GGML_USE_CUBLAS")
+
+        else()
+    endif(LLAMA_CUBLAS)
+    
     
 endfunction(set_cuda project_name)
 
