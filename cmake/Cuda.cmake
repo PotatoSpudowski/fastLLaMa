b5031656c56bff79b872916f93d4052e5b997faa
Amit Singh <amitsingh19975@gmail.com>
2023-04-24 19:31:57 +0530

    fix source path

--- cmake/Cuda.cmake
+++ cmake/Cuda.cmake
@@ -9,7 +9,7 @@ function(set_cuda project_name)
 
             enable_language(CUDA)
 
-            target_link_libraries(${project_name} PRIVATE ggml-cuda.cu ggml-cuda.h CUDA::cudart_static CUDA::cublas_static CUDA::cublasLt_static)
+            target_link_libraries(${project_name} PRIVATE ${CMAKE_SOURCE_DIR}/ggml-cuda.cu ${CMAKE_SOURCE_DIR}/ggml-cuda.h CUDA::cudart_static CUDA::cublas_static CUDA::cublasLt_static)
             target_compile_definitions(${project_name} PRIVATE "GGML_USE_CUBLAS")
 
         endif()
