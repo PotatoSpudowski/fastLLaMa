6725b4cbc9588344df06a58e862ef37671bd38d2
PotatoSpudowski <bahushruth.bahushruth@gmail.com>
2023-04-25 22:11:43 +0530

    Testing

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
 import subprocess
+import sys
+import os
 from setuptools import setup, find_packages, Command
+
+def run_build_python():
+    if not os.path.exists("build.py"):
+        print("Error: build.py not found in the current directory", file=sys.stderr)
+        sys.exit(1)
+
+    return_code = os.system("python build.py -l python > build_logs.txt")
+
+    if return_code != 0:
+        print("Error: build.py execution failed", file=sys.stderr)
+        sys.exit(1)
 
 setup(
     name="fastllama",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
-        "dataclasses",
-        "cpuinfo",
-        # Add any other dependencies your project needs here
+        "numpy==1.24.2",
+        "sentencepiece==0.1.97",
+        "torch==2.0.0",
+        "py-cpuinfo==9.0.0",
+        "inquirer==3.1.3"
     ],
     entry_points={
         "console_scripts": [
@@ -23,3 +38,6 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
 )
+
+# Run build_python.py after the package is installed
+run_build_python()
