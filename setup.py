04eb72728aebae80a8d0534c031e5020751ca9e7
PotatoSpudowski <bahushruth.bahushruth@gmail.com>
2023-04-30 16:06:36 +0530

    testing

--- setup.py
+++ setup.py
@@ -4,28 +4,14 @@ import os
 import shutil
 from setuptools import setup, find_packages, Command
 from setuptools.command.install import install
+from setuptools.command.build_ext import build_ext
 import site
 
 import importlib
 
 
-class CustomInstallCommand(install):
-
+class CustomBuildExtCommand(build_ext):
     def run(self):
-        # Install required packages before running compile.py
-        self.distribution.install_requires = [
-            "numpy>=1.24.2",
-            "py-cpuinfo>=9.0.0",
-            "inquirer>=3.1.3",
-            "cmake>=3.20.2"
-        ]
-
-        # Explicitly install the required packages using subprocess
-        for package in self.distribution.install_requires:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-        # install.run(self)
-
         # Run compile.py after the package is installed
         if not os.path.exists("compile.py"):
             print("Error: compile.py not found in the current directory", file=sys.stderr)
@@ -44,6 +30,29 @@ class CustomInstallCommand(install):
         fastllama_dir = os.path.join(site_packages_dir, "fastLLaMa")
         shutil.copy("build/interfaces/python/pyfastllama.so", fastllama_dir)
 
+        super().run()
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        # Install required packages before running compile.py
+        self.distribution.install_requires = [
+            "numpy>=1.24.2",
+            "py-cpuinfo>=9.0.0",
+            "inquirer>=3.1.3",
+            "cmake>=3.20.2"
+        ]
+
+        # Explicitly install the required packages using subprocess
+        for package in self.distribution.install_requires:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+        # Call the CustomBuildExtCommand to build the extension
+        build_ext_cmd = self.get_finalized_command("build_ext")
+        build_ext_cmd.run()
+
+        super().run()
+        
 setup(
     name="fastllama",
     version="0.5",
@@ -63,6 +72,7 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     cmdclass={
+        'build_ext': CustomBuildExtCommand,
         'install': CustomInstallCommand,
     },
 )
