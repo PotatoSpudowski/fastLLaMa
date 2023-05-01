b83057eb2857e28775855b1a88a19738b5ce694c
Amit Singh <amitsingh19975@gmail.com>
2023-05-01 21:05:42 +0530

    refactor setup.py

--- setup.py
+++ setup.py
@@ -29,16 +29,16 @@ class CustomBuildExtCommand(build_ext):
 class CustomInstallCommand(install):
     def run(self):
         # Install required packages before running compile.py
-        self.distribution.install_requires = [
-            "numpy>=1.24.2",
-            "py-cpuinfo>=9.0.0",
-            "inquirer>=3.1.3",
-            "cmake>=3.20.2"
-        ]
+        # self.distribution.install_requires = [
+        #     "numpy>=1.24.2",
+        #     "py-cpuinfo>=9.0.0",
+        #     "inquirer>=3.1.3",
+        #     "cmake>=3.20.2"
+        # ]
 
-        # Explicitly install the required packages using subprocess
-        for package in self.distribution.install_requires:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+        # # Explicitly install the required packages using subprocess
+        # for package in self.distribution.install_requires:
+        #     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
         # Call the CustomBuildExtCommand to build the extension
         build_ext_cmd = self.get_finalized_command("build_ext")
@@ -47,24 +47,6 @@ class CustomInstallCommand(install):
         super().run()
         
 setup(
-    name="fastllama",
-    version="1.0.0",
-    package_dir={"fastllama": "examples/python/fastllama"},
-    packages=["fastllama"],
-    package_data={"fastllama": ["pyfastllama.so"]},
-    install_requires=[
-        "numpy>=1.24.2",
-        "py-cpuinfo>=9.0.0",
-        "inquirer>=3.1.3",
-        "cmake>=3.20.2"
-    ],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-    ],
     cmdclass={
         'build_ext': CustomBuildExtCommand,
         'install': CustomInstallCommand,
