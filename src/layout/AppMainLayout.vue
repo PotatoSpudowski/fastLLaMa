d766c53d36152979534ee8ab15d138392c06e10c
Amit Singh <amitsingh19975@gmail.com>
2023-05-05 21:06:30 +0530

    remove aside when it's not connected

--- src/layout/AppMainLayout.vue
+++ src/layout/AppMainLayout.vue
@@ -1,7 +1,8 @@
 <template>
     <sp-theme theme="spectrum" color="darkest" scale="medium" class="h-full">
-        <div class="w-full h-full" style="display: grid; grid-template-columns: 15rem 1fr;">
-            <aside v-if="$slots.aside" class="bg-zinc-900 h-full overflow-hidden">
+        <div class="w-full h-full" style="display: grid;"
+            :style="{ 'grid-template-columns': isWebsocketConnected ? '15rem 1fr' : '1fr' }">
+            <aside v-if="$slots.aside && isWebsocketConnected" class="bg-zinc-900 h-full overflow-hidden">
                 <slot name="aside"></slot>
             </aside>
             <main class="h-full overflow-hidden">
@@ -12,5 +13,9 @@
 </template>
 
 <script setup lang="ts">
+import useSocketStore from '@/stores/socketStore';
+import { storeToRefs } from 'pinia';
 
+
+const { isConnected: isWebsocketConnected } = storeToRefs(useSocketStore())
 </script>
