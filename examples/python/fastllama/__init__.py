a7ca3b5795fb9d2d29a022d6f32a5aceb1ca8a11
Bahushruth <bahushruth.cs@quillbot.com>
2023-05-06 15:08:56 +0530

    Created new branch for the websocket server

--- examples/python/fastllama/__init__.py
+++
@@ -1,4 +0,0 @@
-import os;
-from .api import *
-
-set_library_path(os.path.dirname(os.path.abspath(__file__)))
--- scripts/utils/python_info.py
+++
@@ -1,7 +0,0 @@
-import sysconfig
-import platform
-
-lib_path: str = sysconfig.get_path('stdlib')
-include_path: str = sysconfig.get_path('include')
-version: str = platform.python_version()
-print(f'{lib_path};{include_path};{version}', end='')
