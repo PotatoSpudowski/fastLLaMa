e3723a7101e9f0e75fc64dbc2232e1c0c861d36f
Amit Singh <amitsingh19975@gmail.com>
2023-05-08 01:02:18 +0530

    fix width from changing

--- src/layout/AppMainLayout.vue
+++ src/layout/AppMainLayout.vue
@@ -2,7 +2,7 @@
     <sp-theme theme="spectrum" color="darkest" scale="medium" class="h-full">
         <div class="w-full h-full" style="display: flex;">
             <aside v-if="$slots.aside" class="bg-zinc-900 h-full overflow-hidden"
-                :style="{ 'flex-basis': isWebsocketConnected ? '15rem' : '0', 'max-width': isWebsocketConnected ? '15rem' : '0' }"
+                :style="{ 'flex-basis': isWebsocketConnected ? '15rem' : '0', 'min-width': isWebsocketConnected ? '15rem' : '0' }"
                 style="transition: flex-basis 150ms ease-in-out;">
                 <slot name="aside"></slot>
             </aside>
