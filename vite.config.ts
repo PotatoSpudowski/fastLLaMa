22a4cacd3321bf87e6ae23c1df36815ec56ed520
Amit Singh <amitsingh19975@gmail.com>
2023-05-08 20:19:47 +0530

    remove base if in dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue({
-        template: {
-            compilerOptions: {
-                isCustomElement: tag => tag.startsWith('sp-') || tag.startsWith('ion-')
+export default defineConfig((config) => {
+    const { mode } = config;
+    const isDev = mode === 'development';
+    return {
+        plugins: [vue({
+            template: {
+                compilerOptions: {
+                    isCustomElement: tag => tag.startsWith('sp-') || tag.startsWith('ion-')
+                }
             }
-        }
-    })],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    base: '/fastLLaMa/'
+        })],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        base: isDev ? '/' : '/fastLLaMa/'
+    }
 })
