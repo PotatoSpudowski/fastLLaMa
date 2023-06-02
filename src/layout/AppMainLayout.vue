359e57fb1071b9bda05cbcde8a04cf1a02601e36
Stduhpf <stephduh@live.fr>
2023-06-02 17:28:08 +0200

    Add button to hide saves bar

--- src/layout/AppMainLayout.vue
+++ src/layout/AppMainLayout.vue
@@ -1,8 +1,9 @@
 <template>
     <sp-theme theme="spectrum" color="darkest" scale="medium" class="h-full">
         <div class="w-full h-full" style="display: flex;">
+            <button @click="toggleAside()"> &lt </button>
             <aside v-if="$slots.aside" class="bg-zinc-900 h-full overflow-hidden"
-                :style="{ 'flex-basis': isWebsocketConnected ? '15rem' : '0', 'min-width': isWebsocketConnected ? '15rem' : '0' }"
+                :style="{ 'flex-basis': isWebsocketConnected && showAside ? '15rem' : '0', 'min-width': isWebsocketConnected && showAside ? '15rem' : '0' }"
                 style="transition: flex-basis 150ms ease-in-out;">
                 <slot name="aside"></slot>
             </aside>
@@ -16,8 +17,14 @@
 
 <script setup lang="ts">
 import useSocketStore from '@/stores/socketStore';
+import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
 
 const { isConnected: isWebsocketConnected } = storeToRefs(useSocketStore())
+const showAside = ref(false);
+
+function toggleAside(){
+  showAside.value = !showAside.value;
+}
 
 </script>
